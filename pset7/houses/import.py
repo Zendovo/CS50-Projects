8d96482c4035ac939e37a304382aae3e68037a61
Zendovo <zendovo@localhost.localdomain>
2020-01-12 17:42:41 +0530

    pset8 - house

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -1,5 +1,10 @@
 import csv
-import re
+from sys import argv
+import cs50
+
+
+# Create database
+db = cs50.SQL("sqlite:///students.db")
 
 
 def main():
@@ -8,4 +13,34 @@ def main():
         print("Usage: python import.py characters.csv")
         exit(0)
 
-    with open(argv[1], 'r') as 
+
+    with open(argv[1], 'r') as csvfile:
+
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            
+            name = row['name'].split()
+
+            house = row['house']
+            birth = row['birth']
+            
+            if len(name) == 3:
+
+                first_name = name[0]
+                middle_name = name[1]
+                last_name = name[2]
+    
+                db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)",
+                            first_name, middle_name, last_name, house, birth)
+
+            if len(name) == 2:
+
+                first_name = name[0]
+                last_name = name[1]
+
+                db.execute("INSERT INTO students (first, last, house, birth) VALUES (?, ?, ?, ?)",
+                            first_name, last_name, house, birth)
+
+
+main()
