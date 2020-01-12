9d901897a291f373f8e45439e41246dd224c3c9f
Zendovo <zendovo@localhost.localdomain>
2020-01-12 16:54:04 +0530

    pset7

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -1,1 +1,11 @@
-# TODO
+import csv
+import re
+
+
+def main():
+
+    if len(argv) != 2:
+        print("Usage: python import.py characters.csv")
+        exit(0)
+
+    with open(argv[1], 'r') as 
