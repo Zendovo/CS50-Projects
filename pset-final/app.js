ce607ff0275dacf3ba50004e980302e821fe862a
zendovo <kumarh664@gmail.com>
2020-04-17 19:14:32 +0530

    Too much stuff

--- pset-final/app.js
+++ pset-final/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const logger = require('./middleware/logger');
@@ -9,13 +10,16 @@ const app = express();
 // Logger
 // app.use(logger);
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
+// Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.use('/', require('./routes/index'));
+app.use('/', require('./routes/users'));
 
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- pset-final/routes/index.js
+++ pset-final/routes/index.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 
-    res.send('Hello World!');
+    res.render('index', {
+        title: 'Welcome!'
+    });
 
 });
 
