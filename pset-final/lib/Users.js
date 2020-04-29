f89aadf03978031835817bbcc4499803945bd6c5
zendovo <kumarh664@gmail.com>
2020-04-29 17:20:57 +0530

    Table creation

--- pset-final/lib/Users.js
+++ pset-final/lib/Users.js
@@ -23,6 +23,24 @@ function createTable() {
         CONSTRAINT users_pkey PRIMARY KEY(id) )
         WITH (oids = false);`);
 
+    pool.query(`CREATE TABLE IF NOT EXISTS schedules (
+        id BIGSERIAL NOT NULL PRIMARY KEY,
+        user_id UUID NOT NULL,
+        schedule VARCHAR(1024) NOT NULL,
+        name VARCHAR(128) );`)
+
+    pool.query(`CREATE TABLE IF NOT EXISTS friends (
+        id BIGSERIAL NOT NULL PRIMARY KEY,
+        user_id UUID NOT NULL,
+        friend_id UUID NOT NULL,
+        timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP );`);
+
+    pool.query(`CREATE TABLE IF NOT EXISTS friend_requests (
+        id BIGSERIAL NOT NULL PRIMARY KEY,
+        sender_id UUID NOT NULL,
+        receiver_id UUID NOT NULL,
+        timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP );`);
+
     pool.query(`CREATE TABLE IF NOT EXISTS friend_request_code (                                                                    
         user_id UUID NOT NULL PRIMARY KEY,
         code CHAR(6) UNIQUE NOT NULL );`);
